8d63c8be3747e9a6f88ba48293079bf785b04f08
Alan32575 <52725927+Alan32575@users.noreply.github.com>
2019-11-20 19:14:29 -0500

    hopefully built initial staged to add/sub inventory along with table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,4 @@
-/* Database structure & cli-table reference
-Products (ProductName, DepartmentName, Price, StockQuantity)
-var inventory = new Table({ head: ["ID", "Product Name", "Department", "Price", "Quantity in stock"] });
-*/
+
 
 var mysql = require("mysql");
 var prompt = require("prompt");
@@ -10,8 +7,46 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
 	user: "root", 
-	password: "",
+	password: "Darby501!",
 	host: "localhost",
 	port: 3306,
 	database: "Bamazon"
 })
+
+
+var db;
+
+
+function displayAll(){
+	db = {};
+	connection.query("SELECT * FROM Products", function(error, results){
+		if(error){throw error};
+		var inventory = new Table({ head: ["ID", "Product Name", "Department", "Price", "Quantity in stock"]});
+		results.forEach(function(value, index, array){
+			//Store each item
+			var idNum = value.id;
+			var product = value.ProductName;
+			var dept = value.DepartmentName;
+			var price = value.Price;
+			var stock = value.StockQuantity;
+
+					//cli-table
+					var newLine = {};
+					newLine[idNum] = [product, dept, price, stock];
+					db[idNum] = value;
+					inventory.push(newLine);
+				})
+				console.log(inventory.toString());
+			})
+		}
+		
+		//Changes inventory levels
+function changeInventory(id, currentQuantity, addOrSubtract){
+	var newQuantity = currentQuantity + addOrSubtract;
+	var updateText = "UPDATE Products\
+		SET StockQuantity = ?\
+		WHERE id = ?";
+	connection.query(updateText, [newQuantity, id], function(error, results){
+		if(error){throw error};
+	})
+}
